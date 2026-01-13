a502d805a07c598796ef0a0b1a58cdad85530cb0
jjnwaterpolo <jaronieuwenhuisen@gmail.com>
2026-01-13 12:31:37 +0100

    LocalCommit Player Move

--- ProjectFrontiers/Assets/External_Assets/InnerDriveStudios/Utils/Common/Settings.cs
+++
@@ -1,9 +0,0 @@
-namespace InnerDriveStudios.Util
-{
-	public static class Settings 
-	{
-		public const string MENU_PATH = "Utilities/InnerDriveStudios/";
-		public const string TEST_MENU_PATH = "Tests/";
-	}
-}
-
