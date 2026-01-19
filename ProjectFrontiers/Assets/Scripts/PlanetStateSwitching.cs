012dd82348be5929bb5300306a3a463520c52835
jjnwaterpolo <jaronieuwenhuisen@gmail.com>
2026-01-19 14:09:30 +0100

    cutscene for in the lab done

--- ProjectFrontiers/Assets/Scripts/PlanetStateSwitching.cs
+++ ProjectFrontiers/Assets/Scripts/PlanetStateSwitching.cs
@@ -12,38 +12,89 @@ public class PlanetStateSwitching : MonoBehaviour
     public GameObject WastelandParent;
     public GameObject OvergrownParent;
 
+    public GameObject WastelandSwitch;
+    public GameObject OvergrownSwitch;
+
     private int i;
+    public static bool HasRemote;
+
+    private float GlitchDelay = .15f;
 
 
     private void Start()
     {
-   
+        
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown((KeyCode)SwitchPlanetStateKey) || Input.GetMouseButtonDown(1))
+        if (HasRemote == true)
         {
-            i++;
-            //Debug.Log("test click");
-            //Debug.Log(PlayerTracker);
-            //SceneManagement.ChangePlanetState(TargetPlanetState);
-
-            Debug.Log("Planet switch activated " + i + "times");
-            switch (TargetPlanetState)
+            if (Input.GetKeyDown((KeyCode)SwitchPlanetStateKey) || Input.GetMouseButtonDown(1))
             {
-                case "Wasteland":
-                    WastelandParent.SetActive(true);
-                    OvergrownParent.SetActive(false);
-                    TargetPlanetState = "Overgrown";
-                    break;
+                i++;
+                //Debug.Log("test click");
+                //Debug.Log(PlayerTracker);
+                //SceneManagement.ChangePlanetState(TargetPlanetState);
 
-                case "Overgrown":
-                    OvergrownParent.SetActive(true);
-                    WastelandParent.SetActive(false);
-                    TargetPlanetState = "Wasteland";
-                    break;
+                Debug.Log("Planet switch activated " + i + "times");
+                switch (TargetPlanetState)
+                {
+                    case "Wasteland":
+                        WastelandParent.SetActive(true);
+                        OvergrownParent.SetActive(false);
+
+                        WastelandSwitch.SetActive(true);
+                        OvergrownSwitch.SetActive(false);
+
+                        TargetPlanetState = "Overgrown";
+                        break;
+
+                    case "Overgrown":
+                        OvergrownParent.SetActive(true);
+                        WastelandParent.SetActive(false);
+
+                        OvergrownSwitch.SetActive(true);
+                        WastelandSwitch.SetActive(false);
+
+                        TargetPlanetState = "Wasteland";
+                        break;
+                }
             }
         }
+    }
+
+    void WasteOn()
+    {
+        WastelandParent.SetActive(true);
+        OvergrownParent.SetActive(false);
+    }
+
+    void OvergrownOn()
+    {
+        OvergrownParent.SetActive(true);
+        WastelandParent.SetActive(false);
+    }
+
+    public void LabStart()
+    {
+        StartCoroutine("LabCutscene");
+    }
+
+    IEnumerator LabCutscene()
+    {
+        OvergrownOn();
+        yield return new WaitForSeconds(GlitchDelay);
+        WasteOn();
+        yield return new WaitForSeconds(GlitchDelay);
+        OvergrownOn();
+        yield return new WaitForSeconds(GlitchDelay);
+        WasteOn();
+        yield return new WaitForSeconds(GlitchDelay);
+        OvergrownOn();
+        yield return new WaitForSeconds(GlitchDelay);
+        WasteOn();
+        yield return new WaitForSeconds(GlitchDelay);
+        OvergrownOn();
     }
 }
